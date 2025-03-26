f566471fd23fc560dab1b2dae6b8b70abec2f879
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-03-26 22:24:31 +0300

    applied both vanilla css, and tailwind for the landing page

--- simple-habit-tracker/src/pages/LandingPage.jsx
+++ simple-habit-tracker/src/pages/LandingPage.jsx
@@ -9,52 +9,55 @@ import React from 'react';
 const LandingPage = () => {
   return (
     <>
-      <h1>Stay on Track with SimpleHabitTracker - Your Smart Habit Companion!</h1>
-      <p>Track your habits effortlessly and improve your daily routine. <br /> Our intuitive interface and powerful features make it easy to stay on top of your goals.</p>
-      <Link to='/signup' className="f-button" >Sign Up</Link>
-      <p><em>Don't wait-<strong>Sign up today!</strong></em></p>
-      <p>Already have an account? <Link to='/login' className="s-button">Login here!</Link> </p>
 
-      <div>
-        <div> {/*first*/}
+      <div className="headlineSection">
+        <h1>Stay on Track with SimpleHabitTracker - Your Smart Habit Companion!</h1>
+        <p style={{textAlign: "center"}}>Track your habits effortlessly and improve your daily routine. Our intuitive interface <br /> and powerful features make it easy to stay on top of your goals.</p>
+        <p><em>Don't wait-<strong>Sign up today!</strong></em></p>
+        <Link to='/signup' className="f-button" style={{marginBottom: "7px",marginTop: "0px"}}>Sign Up</Link>
+      </div>
+      <p style={{textAlign: "center", marginTop: "0px"}}>Already have an account? <Link to='/login' className="s-button">Login here!</Link> </p>
+      
+      <div className="featureSection">
+        <div className="featureSectionNested"> {/*first*/}
           <div className="icon-container">
             <FontAwesomeIcon icon={faCheck} size="2x" style={{color: "#2A858F"}} />
           </div>
 
           <div>
-            <h2>Easy Tracking</h2>
+            <h2 style={{textAlign: "center"}}><strong>Easy Tracking</strong></h2>
           </div>
 
           <div>
-            <p>Quickly log your habits and monitor <br /> your progress with our user-freindly <br /> interface.</p>
+            <p style={{textAlign: "center"}} >Quickly log your habits and monitor <br /> your progress with our user-freindly <br /> interface.</p>
           </div>
         </div>
 
-        <div> {/*second*/}
+        <div className="featureSectionNested"> {/*second*/}
           <div className="icon-container">
             <FontAwesomeIcon icon={faChartSimple} size="2x" style={{color: "#2A858F"}} />
           </div>
 
           <div>
-            <h2>Detailed Analytics</h2>
+            <h2 style={{textAlign: "center"}}>Detailed Analytics</h2>
           </div>
 
           <div>
-            <p>Access comprehensive analytics to <br /> understand your habits and make <br /> informed decisions.</p>
+            <p style={{textAlign: "center"}}>Access comprehensive analytics to <br /> understand your habits and make <br /> informed decisions.</p>
           </div>
         </div>
 
-        <div> {/*third*/}
+        <div className="featureSectionNested"> {/*third*/}
           <div className="icon-container">
             <FontAwesomeIcon icon={faBell} size="2x" style={{color: "#2A858F"}} />
           </div>
 
           <div>
-            <h2>Timely Reminders</h2>
+            <h2 style={{textAlign: "center"}}>Timely Reminders</h2>
           </div>
 
           <div>
-            <p>Set reminders to keep you on track and never miss a habit again.</p>
+            <p style={{textAlign: "center"}}>Set reminders to keep you on track <br /> and never miss a habit again.</p>
           </div>
         </div>
       </div>
