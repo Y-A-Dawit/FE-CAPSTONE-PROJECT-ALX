0bd430018e3d57f25091f4ba96701af01f00f294
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-03-31 16:48:14 +0300

    implemented inner page navigation between the sign up and login page, and added form validation for password, email, and re-peat password

--- simple-habit-tracker/src/pages/Login.jsx
+++ simple-habit-tracker/src/pages/Login.jsx
@@ -1,37 +1,83 @@
-import React from 'react'
-import './Login.css'
+import React from 'react';
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import './Login.css';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+    /* this code snippet/piece of code is for handling empty form submission, */
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  
+    const handleLoginSubmission = (e) => {
+      e.preventDefault();
+
+      if (!email || !password) {
+        setError('Please fill in all the fields');
+        return;
+      }
+
+      if (!emailRegex.test(email)) {
+        setError('Please enter a valid email');
+        return;
+      }
+
+      if (!passwordRegex.test(password)) {
+        setError("Password must be at least 8 characters long, with 1 uppercase, 1 number, and 1 special character.");
+        return;
+      }
+
+      setError(""); // this one will help clear the error if validation passes
+
+      setTimeout(() => {
+        navigate('/dashboard');
+      }, 450)
+    };
+
   return (
       <>
-        <div className="login-form">
-          <div className="login-html">
-              <div className="group">
-                <h1>Login</h1>
-              </div>
-              <div className="group stack">
-                <label htmlFor="email" className="label" style={{ color: 'black' }}>Email</label>
-                <input id="email" type="text" className="input" />
-              </div>
-              <div className="group stack">
-                <label htmlFor="password" className="label" style={{ color: 'black' }}>Password</label>
-                <input id="password" type="password" className="input" data-type="password" />
-              </div>
-              <div className="checkbox" style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                <input id="keep-signed-in" type="checkbox" className="check" defaultChecked />
-                <label htmlFor="keep-signed-in" style={{ color: 'black'}}>
-                  <span className="icon"></span> Keep me signed in
-                </label>
-              </div>
-              <div className="group stack">
-                <input type="submit" className="button" value="Sign In" style={{ backgroundColor: '#2A858F' }} />
-              </div>
-              <div className="hr" style={{ backgroundColor: '#656565' }}></div>
-              <div className="foot-lnk">
-                <a href="#forgot">Forgot Password?</a>
-              </div>
+        <form onSubmit={handleLoginSubmission}>
+          <div className="login-form">
+            <div className="login-html">
+                <div className="group">
+                  <h1>Login</h1>
+                </div>
+                <div className="group stack">
+                  <label htmlFor="email" className="label" style={{ color: 'black' }}>Email</label>
+                  <input id="email" placeholder="Enter Email" type="email" className="input" onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div className="group stack">
+                  <label htmlFor="password" className="label" style={{ color: 'black' }}>Password</label>
+                  <input id="password" type="password" placeholder="Enter Password" className="input" data-type="password" onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <div className="checkbox" style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                  <input id="keep-signed-in" type="checkbox" className="check" defaultChecked />
+                  <label htmlFor="keep-signed-in" style={{ color: 'black'}}>
+                    <span className="icon"></span> Keep me signed in
+                  </label>
+                </div>
+                <div className="group stack">
+                  <button onClick={handleLoginSubmission} className='button' style={{backgroundColor: '#2A8858F'}}>
+                    Login
+                  </button>
+                </div>
+                <div className="hr" style={{ backgroundColor: '#2A858F' }}></div>
+                <div>
+                  <p style={{fontSize: '14px', fontWeight: 'bold'}}>Don't have an account? <Link to='/signup' className="toSU" >Sign up here!</Link> </p>
+                </div>
+                <div className="foot-lnk">
+                  <a href="#forgot">Forgot Password?</a>
+                </div>
+            </div>
           </div>
-        </div>
+          {error && <p style={{ color: 'red'}}>{error}</p>}
+        </form>
       </>
   )
 }
--- simple-habit-tracker/src/pages/SignUp.jsx
+++ simple-habit-tracker/src/pages/SignUp.jsx
@@ -1,36 +1,93 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import './SignUp.css';
 
 const SignUp = () => {
+      const navigate = useNavigate();
+
+      const [username, setUsername] = useState("");
+      const [email, setEmail] = useState("");
+      const [password, setPassword] = useState("");
+      const [repeatPassword, setRepeatPassword] = useState("")
+      const [error, setError] = useState("");
+    
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  const handleSignUpSubmission = (e) => {
+    e.preventDefault();
+
+    if (!email || !password || !username || !repeatPassword) {
+      setError('Please fill in all the fields');
+      return;
+    }
+
+
+    if (!emailRegex.test(email)) {
+      setError('Please enter a valid email!');
+      return;
+    }
+
+    if (!passwordRegex.test(password)) {
+      setError("Password must be at least 8 characters long, with 1 uppercase, 1 number, and 1 special character.");
+      return;
+    }
+
+    if (password !== repeatPassword) {
+      setError("Passwords do not match!")
+      return;
+    }
+
+    setError("");
+
+    setTimeout (() => {
+      navigate('/dashboard');
+    }, 450)
+  }
+
   return (
         <>
+        <form onSubmit={handleSignUpSubmission}>
           <div className="signUp-form">
             <div className="signUp-html">
-            <div className="group">
-              <label htmlFor="signup-username" className="label" style={{ color: 'black' }}>Username</label>
-              <input id="signup-username" type="text" className="input" />
-            </div>
-            <div className="group">
-              <label htmlFor="signup-password" className="label" style={{ color: 'black' }}>Password</label>
-              <input id="signup-password" type="password" className="input" data-type="password" />
-            </div>
-            <div className="group">
-              <label htmlFor="repeat-password" className="label" style={{ color: 'black' }}>Repeat Password</label>
-              <input id="repeat-password" type="password" className="input" data-type="password" />
-            </div>
-            <div className="group">
-              <label htmlFor="email" className="label" style={{ color: 'black' }}>Email Address</label>
-              <input id="email" type="text" className="input" />
-            </div>
-            <div className="group">
-              <input type="submit" className="button" value="Sign Up" style={{ backgroundColor: '#2A858F' }} />
-            </div>
-            <div className="hr"></div>
-            <div className="foot-lnk">
-              <label htmlFor="tab-1">Already Member?</label>
-            </div>
+              <div className="group">
+                <h1>
+                  Sign Up
+                </h1>
+              </div>
+              <div className="group">
+                <label htmlFor="signup-username" className="label" style={{ color: 'black' }}>Username</label>
+                <input id="signup-username" type="text" className="input" placeholder='Enter Username' onChange={(e) => setUsername(e.target.value)} />
+              </div>
+              <div className="group">
+                <label htmlFor="signup-password" className="label" style={{ color: 'black' }}>Password</label>
+                <input id="signup-password" type="password" className="input" data-type="password" placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)} />
+              </div>
+              <div className="group">
+                <label htmlFor="repeat-password" className="label" style={{ color: 'black' }}>Repeat Password</label>
+                <input id="repeat-password" type="password" className="input" data-type="password" placeholder='Repeat password' onChange={(e) => setRepeatPassword(e.target.value)} />
+              </div>
+              <div className="group">
+                <label htmlFor="email" className="label" style={{ color: 'black' }}>Email Address</label>
+                <input id="email" type="text" className="input" placeholder='Enter Email' onChange={(e) => setEmail(e.target.value)} />
+              </div>
+              <div className="group">
+              <button onClick={handleSignUpSubmission} className='button' style={{backgroundColor: '#2A8858F'}}>
+                  Sign Up
+                </button>
+              </div>
+              <div className="hr" style={{ backgroundColor: '#2A858F' }}></div>
+              <div className="foot-lnk">
+                <p>
+                  <Link to='/login' className='toL'>Already a member?</Link>
+                </p>
+              </div>
             </div>
           </div>
+        {error && <p style={{ color: 'red'}}>{error}</p>}
+        </form>
         </>
   );
 };
