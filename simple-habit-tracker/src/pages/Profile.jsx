1099192cfed10be500f7c7e00f3a99c49bff69a3
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-04-06 18:42:49 +0300

    name change update

--- simple-habit-tracker/src/pages/Profile.jsx
+++ simple-habit-tracker/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './profile.css';
+import './Profile.css';
 
 const Profile = () => {
   const [fullName, setFullName] = useState("");
